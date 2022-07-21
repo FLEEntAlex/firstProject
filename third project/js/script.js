816f1caa0b92ce836deaae43e3a0fb752bdfd928
FLEEnt <FLEEntAlex1@gmail.com>
2022-07-21 21:19:14 +0300

    added tabs and timer

--- third project/js/script.js
+++ third project/js/script.js
@@ -1,101 +1,97 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/main.js");
-/******/ })
-/************************************************************************/
-/******/ ({
+"use strict";
 
-/***/ "./src/js/main.js":
-/*!************************!*\
-  !*** ./src/js/main.js ***!
-  \************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+window.addEventListener("DOMContentLoaded", () => {
+  /*TABS================================================================================================*/
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
 
+  const hideTabContent = () => {
+    tabsContent.forEach((el) => {
+      //el.style.display = "none"; //делаем через классы которые добавили в файл newStyle.css
+      el.classList.add("hide");
+      el.classList.remove("show", "fade");
+    });
+    tabs.forEach((el) => {
+      el.classList.remove("tabheader__item_active");
+    });
+  };
+  const showTabContent = (i = 0) => {
+    //tabsContent[i].style.display = "block"; //так же заменили на классы
+    tabsContent[i].classList.remove("hide"); //
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.add("tabheader__item_active");
+  };
 
+  hideTabContent();
+  showTabContent();
 
-/***/ })
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((el, i) => {
+        if (target === el) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+  /*TIMER==================================================================================================*/
+  const judgmentDay = "2021-08-24";
 
-/******/ });
-//# sourceMappingURL=script.js.map
+  const getTimeRemeaning = (endTime) => {
+    const t = Date.parse(endTime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / 1000 / 60 / 60) % 24),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      t: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  };
+
+  const setTimer = (timer, endTime) => {
+    const time = document.querySelector(timer),
+      days = time.querySelector("#days"),
+      hours = time.querySelector("#hours"),
+      minutes = time.querySelector("#minutes"),
+      seconds = time.querySelector("#seconds"),
+      timeInterval = setInterval(updateTimer, 1000);
+
+    updateTimer();
+
+    function getZero(param) {
+      if (param > 0 && param < 10) {
+        return (param = "0" + param);
+      } else {
+        return param;
+      }
+    }
+
+    function updateTimer() {
+      const t = getTimeRemeaning(endTime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.t <= 0) {
+        clearInterval(timeInterval);
+        days.innerHTML = getZero("00");
+        hours.innerHTML = getZero("00");
+        minutes.innerHTML = getZero("00");
+        seconds.innerHTML = getZero("00");
+        const freebie = document.querySelector("#freebie");
+        freebie.innerHTML = "ХАЛЯВА ЗАКОНЧИЛАСЬ:";
+      }
+    }
+  };
+  setTimer(".timer", judgmentDay);
+});
